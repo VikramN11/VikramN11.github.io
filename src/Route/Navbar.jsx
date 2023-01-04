2662ecd7a7809f21e634386ca09fc9a4248ce4ad
Vikram Navik <vikramnavik98@gmail.com>
2023-01-04 20:24:09 +0530

    navbar change

--- src/Route/Navbar.jsx
+++ src/Route/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div style={{display:"flex", justifyContent : "space-evenly", alignItems:"center", border:"3px solid gray"}}>
+    <div style={{display:"flex", justifyContent : "space-evenly", alignItems:"center", border:"3px solid gray", position:"sticky", top:"0px"}}>
         <div>Home</div>
         <div>About Me</div>
         <div>Skills</div>
