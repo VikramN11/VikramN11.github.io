e125332fa3589e114d9bdac5d8965431c3293a54
Vikram Navik <vikramnavik98@gmail.com>
2023-03-01 00:56:13 +0530

    navbarres

--- src/Route/Navbar.jsx
+++ src/Route/Navbar.jsx
@@ -8,6 +8,13 @@ const Navbar = () => {
       window.open(Vikram_Navik);
     }
 
+    // const toggleButton = document.getElementsByClassName("toggle_button")[0];
+    // const navbarLinks = document.getElementsByClassName("navdiv")[0];
+
+    // toggleButton.addEventListener('click', ()=>{
+    //   navbarLinks.classList.toggle("active");
+    // })
+
   return (
     <div className={styles.navdiv} style={{display:"flex", justifyContent : "space-evenly", alignItems:"center", position:"sticky", top:"0",boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px", fontSize:"2.2rem", borderRadius:"0px 0px 5px 5px", padding:"1rem", color:"#8892b0", backgroundColor:"#0a192f"}}>
         <Link to="home" smooth={true} duration={500}>
@@ -30,11 +37,11 @@ const Navbar = () => {
             <button style={{border:"0.2rem solid #8892b0", padding:"0.5rem", borderRadius:"0.7rem"}}>Resume</button>
           </div>
         </a>
-        <a href="#" className={styles.toggle_button}>
+        {/* <a href="#" className={styles.toggle_button}>
           <span className={styles.bar}></span>
           <span className={styles.bar}></span>
           <span className={styles.bar}></span>
-        </a>
+        </a> */}
     </div>
   )
 }
